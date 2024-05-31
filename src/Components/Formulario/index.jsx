d6bd794cfb0630296be15627084376b820b7cc2a
Lucas Mario <lucasmariocontato@gmail.com>
2024-05-31 13:21:32 -0300

    ajuste

--- src/Components/Formulario/index.jsx
+++ src/Components/Formulario/index.jsx
@@ -3,55 +3,127 @@ import { useState, useEffect } from "react"
 import styles from './Formulario.module.css';
 
 const Formulario = () => {
-    const [altura, setAltura] = useState(0);
-    const [peso, setPeso] = useState(0);
-    const [nome, setNome] = useState('');
+    const [altura, setAltura] = useState("");
+    const [peso, setPeso] = useState("");
+    const [IMCData, setIMCData] = useState(null);
 
     useEffect(() => {
-        console.log("o estado mudou");
-    });
+        if (setIMCData) {
+            console.log("o estado mudou");
+        }
+       
+    }, [IMCData]);
 
-    const alteraNome = (evento) => {
-     //   setNome(evento.target.value);
-        setNome(estadoAnterior => {
-          //  setNome(evento.target.value);
-            return evento.target.value;
-        })
-    }
+    const handlePesoChange = (e) => {
+        const valor = e.target.value;
+        if (valor < 0) {
+          setPeso("");
+        } else {
+          setPeso(valor);
+        }
+      };
+    
+      const handleAlturaChange = (e) => {
+        const valor = e.target.value;
+        if (valor < 0) {
+          setAltura("");
+        } else {
+          setAltura(valor);
+        }
+      };
+    
 
-const renderizaResultado = () => {
-    const soma = altura + peso;
-    const media = soma /2;
-
-   
-    if (media >= 150) {
-        return (
-            <p>Olá {nome}, você não está na média recomendada</p>
-        )
-    } else {
-        return (
-            <p>Olá {nome}, Você está na média recomendada</p>
-        )
-    }
+      const resultadoIMC = () => {
+        try {
+          const pesoNumber = parseFloat(peso.replace(",", "."));
+          const alturaNumber = parseFloat(altura.replace(",", ".")) / 100;
+    
+          if (isNaN(pesoNumber) || isNaN(alturaNumber) || pesoNumber < 0 || alturaNumber < 0) {
+            throw new Error("Ops... você precisa preencher os campos com números válidos");
+          }
+    
+          if (pesoNumber < 2 || pesoNumber > 500) {
+            throw new Error("Ops... o peso precisa ser maior que 2Kg e menor que 500kg");
+          }
+    
+          if (alturaNumber < 0.5 || alturaNumber > 2.5) {
+            throw new Error("Ops... a altura precisa ser maior que 50cm e menor que 2,5m");
+          }
+    
+          const IMC = calcularIMC(pesoNumber, alturaNumber).toFixed(2);
+          const IMCResultText = msgTexto(IMC);
+    
+          setIMCData({
+            peso: pesoNumber,
+            altura: alturaNumber,
+            IMC: parseFloat(IMC, 2),
+            result: IMCResultText,
+          });
+    
+    
+          setPeso("");
+          setAltura("");
+        } catch (error) {
+          alert(error.message);
         
-}
-
-    return (
-        <form>
-        <div className={styles.altura}> 
-        <input className={styles.botao}  type="text" placeholder="Seu nome" onChange={alteraNome} />
-        <input className={styles.botao}type="number" placeholder="altura" onChange={({ target}) => setAltura(parseInt(target.value))} />
-        <input className={styles.botao}type="number" placeholder="peso" onChange={evento => setPeso(parseInt(evento.target.value))} />
-        <ul>
-            {[1,2,3,4,5].map(item => (
-    <li key={item}></li>
-))}
-        </ul>
-   {renderizaResultado()}
-   </div>
-    </form>
-    )
-}
-
-
-export default Formulario
+          setPeso("");
+          setAltura("");
+        }
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        resultadoIMC();
+      };
+    
+      return (
+        <>
+          <form onSubmit={handleSubmit} className={styles.formulario}>
+            <div id="contai">
+              <div id="container1">
+                <label htmlFor="peso">Peso (Kg)</label>
+                <input
+                  type="number"
+                  id="peso"
+                  value={peso}
+                  onChange={handlePesoChange}
+                />
+              </div>
+              <div id="container2">
+                <label htmlFor="altura">Altura (cm)</label>
+                <input
+                  type="number"
+                  id="altura"
+                  value={altura}
+                  onChange={handleAlturaChange}
+                />
+              </div>
+            </div>
+    
+            <button type="submit">Calcular</button>
+          </form>
+          {IMCData ? (
+            <table className="table blocoTab">
+              <tbody>
+                <tr>
+                  <td id="item-title">Peso</td>
+                  <td id="item-title">Altura</td>
+                  <td id="item-title">IMC</td>
+                  <td id="item-title">Resultado</td>
+                </tr>
+                <tr>
+                  <td>{IMCData.peso}</td>
+                  <td>{IMCData.altura}</td>
+                  <td>{IMCData.IMC}</td>
+                  <td>{IMCData.result}</td>
+                </tr>
+              </tbody>
+            </table>
+          ) : (
+            <p>Saiba agora se está no seu peso ideal!</p>
+          )}
+        </>
+      );
+    };
+    
+    export default Formulario;
