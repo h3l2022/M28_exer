a11beaa96f2294fed97c66c89b1c40ffafb84f7c
Lucas Mario <lucasmariocontato@gmail.com>
2024-06-03 14:10:55 -0300

    correcao

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Perfil from "./Components/Perfil";
 import Formulario from "./Components/Formulario";
-import Tabela from "./Components/Tabela"; 
 
 function App() {
   return (
     <>
-    <Tabela />
     <Perfil nome="Calcule seu IMC" endereco="https://static.vecteezy.com/ti/vetor-gratis/p3/20717578-placa-simbolo-saude-logotipo-hospital-vermelho-cruz-e-a-coracao-icone-e-uma-simbolo-do-saude-vetor.jpg" />
     
     <Formulario />
